9d875723d292524bc980c88df040b4b8323c7318
Hatem Nassrat <hnassrat@gmail.com>
2014-07-20 10:39:10 -0300

    Adding base package.

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+version='0.0.1'
+
+setup(
+    name='urban_dictionary_words',
+    long_description='Urban Dictionary Word Downloader',
+    author='Hatem Nassrat',
+    zip_safe=False,
+    version=version,
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    install_requires=[
+        'beautifulsoup',
+    ],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
